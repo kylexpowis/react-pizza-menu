0439210d8bc1ad521f8fa8d0c00f5cd5865d12e5
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-08-28 14:35:43 +0100

    add pizzas function to li and to classname for grid design

--- src/index.js
+++ src/index.js
@@ -72,7 +72,11 @@ function Menu() {
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <div></div>
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => (
+          <Pizza pizzaObj={pizza} key={pizza.name} />
+        ))}
+      </ul>
 
       {/*<Pizza
         name="Pizza Spinaci"
@@ -112,14 +116,14 @@ function Footer() {
 function Pizza(props) {
   console.log(props);
   return (
-    <div className="pizza">
-      <img src={props.photoName} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p> {props.ingredients} </p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p> {props.pizzaObj.ingredients} </p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
