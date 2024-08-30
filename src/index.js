6563ae25366f6e09732a7839287ec7bddcb6ce9f
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-08-30 12:42:41 +0100

    add ternary logic to menu and footer

--- src/index.js
+++ src/index.js
@@ -68,15 +68,23 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  // const pizzas = [];
+
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 ? (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      ) : (
+        <p> We're updatng our menu, please come back later! </p>
+      )}
 
       {/*<Pizza
         name="Pizza Spinaci"
@@ -97,7 +105,7 @@ function Menu() {
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 8;
+  const openHour = 12;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
@@ -107,10 +115,21 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {" "}
-      {new Date().toLocaleTimeString()} We're currently open!{" "}
+      {isOpen ? (
+        <div>
+          <p>
+            We're open until {closeHour}:00. Come visit us or order online.{" "}
+          </p>
+          <button className="btn">Order Now</button>
+        </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00{" "}
+        </p>
+      )}
     </footer>
   );
+
   // return React.createElement("footer", null, "We're Open!");
 }
 function Pizza(props) {
