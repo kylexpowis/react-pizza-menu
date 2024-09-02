b268f38c45d3fccfdfac8d5619e581f605f81ac6
Kyle Powis <kylepowis@Kyles-MacBook-Air.local>
2024-09-02 13:52:20 +0100

    fix footer text

--- src/index.js
+++ src/index.js
@@ -145,10 +145,7 @@ function Footer() {
 function Order({ closeHour, openHour }) {
   return (
     <div className="order">
-      <p>
-        We're open from {openHour}s until {closeHour}:00. Come visit us or order
-        online.{" "}
-      </p>
+      <p>We're open until {closeHour}:00. Come visit us or order online. </p>
       <button className="btn">Order Now</button>
     </div>
   );
